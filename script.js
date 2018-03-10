064bb2eaf0fdee19f6d4561f85085ffd6ade9bb2
Student <student@Admins-MacBook-Air.local>
2018-03-10 13:00:25 -0800

    Shooter

--- script.js
+++ script.js
@@ -39,6 +39,12 @@ var alienBulletY;
  * select(), and adds event listeners to those elements. Sets initial values of
  * variables by calling resetGame().
  */
+ function setup(){
+   canvas = createcanvas(500, 400);
+   background(20, 30, 40);
+   gameScreen = select("#game-screen");
+   canvas.parent(gameScreen);
+ }
 
 
 /*
